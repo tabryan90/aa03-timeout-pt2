9989f6eb7d7e80289d9c3ea8f3a1fc92e2d101ac
Alex Bryan <tabryan1990@gmail.com>
2024-02-27 22:39:28 -0500

    problem 06 solved

--- problems/06-dynamic-interval-count.js
+++ problems/06-dynamic-interval-count.js
@@ -1,13 +1,13 @@
 /***********************************************************************
 Write a function, `dynamicIntervalCount`, that accepts a callback, a delay
-in milliseconds, and an optional amount as arguments. The function should 
-set an interval with the given callback and delay. If an amount argument 
-is passed, the interval should be cleared after the callback has been 
+in milliseconds, and an optional amount as arguments. The function should
+set an interval with the given callback and delay. If an amount argument
+is passed, the interval should be cleared after the callback has been
 called 'amount' number of times. If an amount argument is not passed,
 the interval should not be cleared and `dynamicIntervalCount` should instead
 return the Timeout object for the interval.
 
-In addition to Mocha, we recommend that you test your code manually using 
+In addition to Mocha, we recommend that you test your code manually using
 node with the examples below.
 
 Examples:
@@ -24,9 +24,32 @@ const timeoutObject = dynamicIntervalCount(function() {
 console.log(timeoutObject); // Timeout { ... }
 ***********************************************************************/
 
+debugger
 function dynamicIntervalCount(cb, delay, amount) {
-  // Your code here 
+  if (amount === 0) {
+    let timeOut = setTimeout(cb, delay);
+    return timeOut;
+  } else {
+    const intObj = setInterval(function () {
+      setTimeout(cb, delay);
+      amount--;
+      if (amount === 0) {
+        clearInterval(intObj);
+      }
+    }, amount);
+  }
 }
+
+dynamicIntervalCount(function() {
+  console.log('hi');
+}, 500, 3); // prints 'hi' at 500ms intervals a total of 3 times
+
+
+// const timeoutObject = dynamicIntervalCount(function() {
+//   console.log('hi');
+// }, 500); // prints 'hi' at 500ms intervals indefinitely
+
+// console.log(timeoutObject); // Timeout { ... }
 
 /**************DO NOT MODIFY ANYTHING UNDER THIS  LINE*****************/
 try {
